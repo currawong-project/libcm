032d359a90d51ade7c3311bc1960aaf3b645f3a3
kevin <kevin@administrators-MacBook.local>
2015-07-03 12:36:27 -0400

    cmMath.h/c : Added cmLFSR() and cmGenGoldCode().

--- cmMath.h
+++ cmMath.h
@@ -74,4 +74,41 @@ bool cmIsCloseF( float    x0, float    x1, double eps );
 bool cmIsCloseI( int      x0, int      x1, double eps );
 bool cmIsCloseU( unsigned x0, unsigned x1, double eps );
 
+//=================================================================
+// Run a length 'lfsrN' linear feedback shift register (LFSR) for 'yN' iterations to
+// produce a length 'yN' bit string in yV[yN].
+// 'lfsrN' count of bits in the shift register range: 2<= lfsrN <= 32.
+// 'tapMask' is a bit mask which gives the tap indexes positions for the LFSR. 
+// The least significant bit corresponds to the maximum delay tap position.  
+// The min tap position is therefore denoted by the tap mask bit location 1 << (lfsrN-1).
+// A minimum of two taps must exist.
+// 'seed' sets the initial delay state.
+// 'yV[yN]' is the the output vector
+// 'yN' is count of elements in yV.
+// The function resturn kOkAtRC on success or kInvalidArgsRCRC if any arguments are invalid.
+// /sa cmLFSR_Test.
+void   cmLFSR( unsigned lfsrN, unsigned tapMask, unsigned seed, unsigned* yV, unsigned yN );
+
+// Example and test code for cmLFSR() 
+bool cmLFSR_Test();
+
+
+// Generate a set of 'goldN' Gold codes using the Maximum Length Sequences (MLS) generated
+// by a length 'lfsrN' linear feedback shift register.
+// 'err' is an error object to be set if the the function fails.
+// 'lfsrN' is the length of the Linear Feedback Shift Registers (LFSR) used to generate the MLS.
+// 'poly_coeff0' tap mask for the first LFSR.
+// 'coeff1' tap mask the the second LFSR.
+// 'goldN' is the count of Gold codes to generate. 
+// 'yM[mlsN', goldN] is a column major output matrix where each column contains a Gold code.
+// 'mlsN' is the length of the maximum length sequence for each Gold code which can be
+// calculated as mlsN = (1 << a->lfsrN) - 1.
+// Note that values of 'lfsrN' and the 'poly_coeffx' must be carefully selected such that
+// they will produce a MLS.  For example to generate a MLS with length 31 set 'lfsrN' to 5 and
+// then select poly_coeff from two different elements of the set {0x12 0x14 0x17 0x1B 0x1D 0x1E}.
+// See http://www.ece.cmu.edu/~koopman/lfsr/index.html for a complete set of MSL polynomial
+// coefficients for given LFSR lengths.
+// Returns false if insufficient balanced pairs exist.
+bool   cmGenGoldCodes( unsigned lfsrN, unsigned poly_coeff0, unsigned poly_coeff1, unsigned goldN, int* yM, unsigned mlsN  );
+
 #endif
