f028c892ad7bf382af5fff320fa34f14579df8bc
kevin <kevin@larke.org>
2017-12-17 13:41:25 -0500

    cmRptFile.c : Initial commit.

---
+++ cmRptFile.h
@@ -0,0 +1,30 @@
+#ifndef cmRptFile_h
+#define cmRptFile_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  enum
+  {
+    kOkRfRC = cmOkRC,
+    kFileFailRfRC
+  };
+
+  typedef unsigned   cmRfRC_t;
+  typedef cmHandle_t cmRptFileH_t;
+
+  extern cmRptFileH_t cmRptFileNullHandle;
+
+  cmRfRC_t cmRptFileCreate( cmCtx_t* ctx, cmRptFileH_t* hp, const cmChar_t* printFn, const cmChar_t* errorFn );
+  cmRfRC_t cmRptFileClose( cmRptFileH_t* hp );
+  
+  bool     cmRptFileIsValid( cmRptFileH_t h );
+    
+  cmRpt_t* cmRptFileRpt( cmRptFileH_t h );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
